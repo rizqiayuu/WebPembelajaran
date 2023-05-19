8fb20e5e78bc1e50c9dfb5418327c05c3951686c
rizqiayuu <127472061+rizqiayuu@users.noreply.github.com>
2023-05-19 11:03:42 +0700

    Add files via upload

---
+++ IILatihanHelloWorld.js
@@ -0,0 +1,20 @@
+// tampilan awal
+document.writeln("HELLO WORLD");
+document.writeln("</br>")
+document.writeln("SELAMAT DATANG")
+document.writeln("</br>")
+document.writeln(0B1110101010)
+document.writeln("</br>")
+
+// escape sequence
+document.writeln("<textarea cols='130' rows='12'>")
+document.writeln("\n Name: Sekar Ayu Rizqi Utami")
+document.writeln("\n Alamat: Tuban, Jawa Timur")
+document.writeln("\n TTL: Tuban, 16 November 2002")
+document.writeln("\n Prodi: S1 Pendidikan Administrasi Perkantoran")
+
+document.writeln("</textarea>")
+document.writeln("</br>")
+document.writeln("\n TERIMA KASIH")
+document.writeln("</br>")
+document.writeln("\n &#x1F90D")
